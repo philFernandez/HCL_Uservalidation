778f550724b12bb263c0030261baf741f0638928
Phil Fernandez <norcal618@gmail.com>
2020-12-23 19:33:48 -0800

    add another user

--- src/main/java/com/service/UserService.java
+++ src/main/java/com/service/UserService.java
@@ -4,12 +4,20 @@ import com.dao.Dao;
 import com.model.LoginModel;
 
 public class UserService implements Dao {
-    private static final String USER = "John";
-    private static final String PASS = "1234";
+    // Provided two hardcoded users to demostrate dynamic dashboard functionality
+    private static final String USER1 = "John";
+    private static final String PASS1 = "1234";
+    private static final String USER2 = "Joe";
+    private static final String PASS2 = "4321";
 
     @Override
     public boolean login(LoginModel loginModel) {
-        return loginModel.getPassword().equals(PASS)
-                && loginModel.getUsername().equals(USER);
+        // return true if user name is "John" AND password is "1234" OR
+        // return true if user name is "Joe" AND password is "4321"
+        return (loginModel.getPassword().equals(PASS1)
+                && loginModel.getUsername().equals(USER1))
+                || (loginModel.getPassword().equals(PASS2)
+                        && loginModel.getUsername().equals(USER2));
     }
+
 }
