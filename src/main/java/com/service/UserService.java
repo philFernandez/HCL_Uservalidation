cd97312651bb8a30fbd441c7a6689927c66e16a1
Phil Fernandez <norcal618@gmail.com>
2020-12-23 19:16:44 -0800

    we gud

--- src/main/java/com/service/UserService.java
+++ src/main/java/com/service/UserService.java
@@ -4,16 +4,12 @@ import com.dao.Dao;
 import com.model.LoginModel;
 
 public class UserService implements Dao {
-    private static final String USER = "John123";
-    private static final String PASS = "123abc";
+    private static final String USER = "John";
+    private static final String PASS = "1234";
 
     @Override
     public boolean login(LoginModel loginModel) {
-        if (loginModel.getPassword().equals(PASS)
-                && loginModel.getUsername().equals(USER)) {
-            return true;
-        }
-        return false;
+        return loginModel.getPassword().equals(PASS)
+                && loginModel.getUsername().equals(USER);
     }
-
 }
--- target/uservalidation/logged_in.jsp
+++ target/uservalidation/logged_in.jsp
@@ -4,14 +4,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Dashboard</title>
 </head>
 
 <body>
     <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
         <c:choose>
             <c:when test="${not empty username}">
-                <h1>Welcome to your Dashboard <%=session.getAttribute("username")%> !</h1>
+                <h1>Welcome to your Dashboard <%=session.getAttribute("username")%>!</h1>
                 <br>
                 <br>
                 <br>
